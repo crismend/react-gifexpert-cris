ad3bd928694cd1fd7ee9e10bfb23cdf014b2ca15
Cristian Mendoza <cfmendoza.audit@gmail.com>
2025-01-06 04:07:20 -0500

    actualizacion Apikey

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,13 +1,13 @@
 // EqRywIaAut7DkhB0ni7cus5fXAMiNjNh
 
-export const getGif = async( category ) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=EqRywIaAut7DkhB0ni7cus5fXAMiNjNh&q=${ category }&limit=10`
-  const resp = await fetch( url );
-  const {data} = await resp.json();
+export const getGif = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=CdoCvUwWWCA9a0CEVxNSI6ZltSiOCCYm&q=${category}&limit=10`
+  const resp = await fetch(url);
+  const { data } = await resp.json();
 
-  const gifs = data.map( img => ({
+  const gifs = data.map(img => ({
     id: img.id,
-    title: img.title, 
+    title: img.title,
     url: img.images.downsized_medium.url
   }));
 
